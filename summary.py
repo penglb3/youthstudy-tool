de27002c947d139185dc10e6b3a329589d564f17
penglb3 <58880775+penglb3@users.noreply.github.com>
2022-10-11 16:01:11 +0800

    Refactor to suit my needs

--- summary.py
+++ summary.py
@@ -2,51 +2,34 @@ import requests,json,main,time,os,re
 with open('result.json','r',encoding='utf8') as origin_file:
     origin=origin_file.read()
 origin=json.loads(origin)
-pushdata={}
+pushdata={'desp':'', 'title':''}
 config=main.config
 #推送渠道
 # pushdata['channel']=config['push']['channel']
 # pushdata['template']='html'
 
-# 具体请查看pushplus api文档https://www.pushplus.plus/doc/guide/api.html
-token=''
-
-if token == '':
-    try:
-        token=os.environ['PUSHTOKEN']
-    except:
-        pass
-LatestStudy=json.loads(requests.get('https://youthstudy.12355.net/saomah5/api/young/chapter/new',headers=main.headers).text)
-StudyId=re.search('[a-z0-9]{10}',LatestStudy['data']['entity']['url']).group(0)
-StudyName=LatestStudy['data']['entity']['name']
-Finishpage='<a href="'+'https://finishpage.dgstu.tk/?id='+StudyId+'&name='+StudyName+'">（伪）当前期完成页</a><br>'
-pushdata['desp']=Finishpage
+try:
+    token=os.environ['PUSHTOKEN']
+except:
+    config['push']['push'] = 'no'
+    print('Token读取失败，不再推送！')
+    pass
 
 time.sleep(60)#平台统计有延迟
-errorcount=0
 for member in origin:
-    if member['status']== 'error':
-        errorcount+=1
-        pushdata['desp']+='<b>mid或X-Litemall-Token:</b>'+member['member']+'<br><b>状态:</b>'+'执行出错'+'<br>'
-        continue
     XLtoken=main.ConverMidToXLToken(member['member'])
     profile=main.GetProfile(XLtoken)
     score_now=profile.score()
     score_add=score_now-member['score']
-    if score_now < 100:
-        score_need=100-score_now
-    elif score_now < 200:
-        score_need=200-score_now
-    elif score_now < 500:
-        score_need=500-score_now
-    elif score_now < 1000:
-        score_need=1000-score_now
-    elif score_now < 5000:
-        score_need=5000-score_now
-    else:
-        score_need=0
-    member['result']+='<br>此次执行增加了<b>'+str(score_add)+'</b>积分'+'<br>当前为<b>'+profile.medal()+'</b>，距离下一徽章还需<b>'+str(score_need)+'</b>积分<br>'
-    pushdata['desp']+='<b>mid或X-Litemall-Token:</b>'+member['member']+'<br><b>名称:</b>'+member['name']+'<br>'+member['result']+'<br>'
+    score_thrsh = [100, 200, 500, 1000, 5000]
+    score_need = 0
+    for threshold in score_thrsh:
+        if score_now < threshold:
+            score_need = threshold - score_now
+            break
+    member['result']+=f'此次执行增加了{str(score_add)}积分，当前为{profile.medal()}，距离下一徽章还需{str(score_need)}积分\n'
+    if config['dailycheckin'] == 'yes' or config['studychannel'] == 'yes' or config['answer_questions'] == 'yes'
+        pushdata['desp'] += f"名称:{member['name']}: {member['result']}"
 
 #检查token
 if ('token' in locals().keys()) == True:
@@ -54,19 +37,15 @@ if ('token' in locals().keys()) == True:
 else:
     exit('+===============+\n| Token未定义! |\n+===============+')
 
-if errorcount!=len(main.memberlist):
-    titledone=False
-    for i in origin:
-        if (i['status']!='error') and (i['status']!='passed'):
-            if titledone==False:
-                pushdata['title']='['+str(len(main.memberlist)-errorcount)+'/'+str(len(main.memberlist))+']'+i['status']+'啦'
-                titledone=True#若有打卡成功的则锁定标题
-        else:
-            if titledone==False:
-                pushdata['title']='['+str(len(main.memberlist)-errorcount)+'/'+str(len(main.memberlist))+']'+'积分任务执行完毕'
-else:
-    pushdata['title']='任务执行失败'
-    pushdata['desp']='所有mid或X-Litemall-Token皆打卡失败'
+failed_members = set(mem['name'] for mem in origin if mem['status'] == 'error')
+n_member = len(main.memberlist)
+n_success = n_member - len(failed_members)
+pushdata['title'] = f'自动智慧团建：({n_success}/{n_member})人已完成'
+if errorcount > 0:
+    if n_success == 0:
+        pushdata['desp'] += '所有人任务均失败'
+    else:
+        pushdata['desp'] += f'失败名单：{failed_members}'
 
 #向Server酱出推送请求
 try:
